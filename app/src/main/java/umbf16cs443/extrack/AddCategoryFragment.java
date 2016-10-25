0eadd89471ceebb81ec2bec238148e16ac82df72
kwokin <kwokinou@gmail.com>
2016-10-24 22:30:39 -0400

    Modified Category fragment to enable Search and Add features

---
+++ app/src/main/java/umbf16cs443/extrack/AddCategoryFragment.java
@@ -0,0 +1,34 @@
+package umbf16cs443.extrack;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import umbf16cs443.extrack.R;
+
+public class AddCategoryFragment extends Fragment {
+
+    //initial set up to prompt user to input info for new category
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        setHasOptionsMenu(true); //to show "save" action button
+        return inflater.inflate(R.layout.add_a_category, container, false);
+    }
+
+    //set up "save" action button
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
+        inflater.inflate(R.menu.save_action_btn, menu);
+        super.onCreateOptionsMenu(menu,inflater);
+    }
+
+    //need to implement onOptionsItemSelected to process "save".
+    // An Expense object will be created and added to database.
+}
--- app/src/main/java/umbf16cs443/extrack/ViewCatFragment.java
+++ app/src/main/java/umbf16cs443/extrack/ViewCatFragment.java
@@ -2,9 +2,11 @@ package umbf16cs443.extrack;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.ListFragment;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 
 
@@ -40,4 +42,36 @@ public class ViewCatFragment extends ListFragment {
         inflater.inflate(R.menu.view_action_btns, menu);
         super.onCreateOptionsMenu(menu,inflater);
     }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+
+            //user clicked on add an expense action button
+            case R.id.add:
+                //update framelayout with addCatFrag
+                AddCategoryFragment addCatFrag = new AddCategoryFragment();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_container, addCatFrag);
+                transaction.addToBackStack(null);
+                transaction.commit();
+                break;
+
+            //user clicked on search an expense action button
+            case R.id.search:
+                //update framelayout with searchCatFrag
+                SearchCategoryFragment searchExpFrg = new SearchCategoryFragment();
+                FragmentTransaction transaction1 = getFragmentManager().beginTransaction();
+                transaction1.replace(R.id.fragment_container, searchExpFrg);
+                transaction1.addToBackStack(null);
+                transaction1.commit();
+                break;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
+
 }
