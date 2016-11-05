790b20c38c6e09d101f9a90796485c1470dccc8f
kwokinou <kwokinou@gmail.com>
2016-11-04 20:35:33 -0400

    clicking on save will return to previous activity

--- app/src/main/java/umbf16cs443/extrack/AddCatActivity.java
+++ app/src/main/java/umbf16cs443/extrack/AddCatActivity.java
@@ -3,6 +3,7 @@ package umbf16cs443.extrack;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.MenuItem;
 
 /**
  * Created by kwokin on 10/30/2016.
@@ -20,6 +21,22 @@ public class AddCatActivity extends AppCompatActivity {
     //include save button in menu bar
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.save_action_btn, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+
+            //user clicked on save to store Category object
+            //return to activity showing all Categories
+            case R.id.save:
+                finish();
+                break;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
         return true;
     }
 }
