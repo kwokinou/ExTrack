da45aca985405f385efe8b89be15950d3ccfce6b
kwokin <kwokinou@gmail.com>
2016-12-13 22:23:31 -0500

    Kind of get started on the Stats UI

--- app/src/main/java/umbf16cs443/extrack/StatsHeadlineFragment.java
+++ app/src/main/java/umbf16cs443/extrack/StatsHeadlineFragment.java
@@ -2,19 +2,24 @@ package umbf16cs443.extrack;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.ListFragment;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import umbf16cs443.extrack.db.DBHelper;
 
 /**
  * Created by kwokin on 12/12/2016.
  */
 public class StatsHeadlineFragment extends ListFragment {
+
     String[] StatsHeadlines = {
             "Grand Total",
-            "All Expenses of a Category",
+            "All Category Totals",
             "Expenses in a Time Frame",
             "Events that Share an Expense"
     };
@@ -32,7 +37,35 @@ public class StatsHeadlineFragment extends ListFragment {
     }
 
     @Override
+    public void onStart() {
+        super.onStart();
+
+        // When in two-pane layout, set the listview to highlight the selected list item
+        // (We do this during onStart because at the point the listview is available.)
+        //getListView().setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        getListView().setSelector(R.drawable.pressedbuttonshape);
+    }
+
+    @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
-        Toast.makeText(getActivity().getApplicationContext(),String.valueOf(position),Toast.LENGTH_LONG).show();
+
+        StaticsActivity stats = (StaticsActivity) getActivity();
+
+        switch (position){
+            case 0:
+                Toast.makeText(getActivity().getApplicationContext(), "Grand Total is $" + stats.getDB().getGrantTotal(), Toast.LENGTH_LONG).show();
+                break;
+
+            case 1:
+                CategoryTotalsFrag list = new CategoryTotalsFrag();
+
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.stats_container, list);
+                transaction.commit();
+                break;
+
+            default:
+                Toast.makeText(getActivity().getApplicationContext(), "Invalid Entry", Toast.LENGTH_LONG).show();
+        }
     }
 }
